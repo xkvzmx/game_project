c39c9f73ea1a7064ccb33a843a8d00ec41a4b239
Michał Kaźmierczak <252975@student.pwr.edu.pl>
2021-06-07 21:15:53 +0200

    ball added; shooting

--- gameplay.py
+++ gameplay.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import os.path
+import random
 
 #-----------------------------------------------------------------------
 # Program parameters
@@ -33,6 +34,10 @@ def loadImage(name, useColorKey=False):
 #-----------------------------------------------------------------------------
 # Object classes
 #-----------------------------------------------------------------------------
+
+#-----------------------------------
+# MyPlayer class
+#-----------------------------------
 class MyPlayer(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -57,11 +62,44 @@ class MyPlayer(pygame.sprite.Sprite):
         elif self.rect.bottom >= 450:
             self.rect.bottom = 450
 
+#-----------------------------------
+# Ball class
+#-----------------------------------
+class Ball(pygame.sprite.Sprite):
+    def __init__(self,other):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = loadImage("ball03.jpg",True)
+        self.rect = self.image.get_rect()
+        self.rect.center = (other.rect.center)
+        self.isdribble = True
+        self.shoot = False
+        self.x_velocity = 0
+        self.y_velocity = 0
+
+    def update(self):
+        if self.isdribble == False:
+            self.kill()
+        elif self.shoot == True:
+            self.rect.move_ip((self.x_velocity,self.y_velocity))
+        else:
+            self.rect.move_ip((self.x_velocity,self.y_velocity))
+
+            if self.rect.left < 133:
+                self.rect.left = 133
+            elif self.rect.right > 1060:
+                self.rect.right = 1060
+
+            if self.rect.top <= 365:
+                self.rect.top = 365
+                self.y_velocity = 5
+            elif self.rect.bottom >= 450:
+                self.rect.bottom = 450
+                self.y_velocity = -5
 
 #-----------------------------------------------------------------------------
 # Proper program
 #-----------------------------------------------------------------------------
-
+pygame.init()
 
 # create game window
 screen = pygame.display.set_mode((SCREEN_SIZE))
@@ -72,9 +110,14 @@ background_image = loadImage("court130.jpg")
 screen.blit(background_image,(0,0))
 
 # initialize player
-myplayerSprite = pygame.sprite.RenderClear()    #container box
+myplayerSprite = pygame.sprite.RenderClear()    #player container
 myplayer = MyPlayer()                           #create player
 myplayerSprite.add(myplayer)                    #add player
+
+# initialize ball
+myballSprite = pygame.sprite.RenderClear()      #ball container
+myball = Ball(myplayer)                         #create ball
+myballSprite.add(myball)                        #add ball
 
 # initialize time and running status
 clock = pygame.time.Clock()
@@ -88,29 +131,62 @@ while running:
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-            elif event.key == K_LEFT:
+            elif event.key == K_LEFT and myball.shoot == False:
                 myplayer.x_velocity = -4
-            elif event.key == K_RIGHT:
+                myball.x_velocity = -4
+            elif event.key == K_RIGHT and myball.shoot == False:
                 myplayer.x_velocity = 4
-            elif event.key == K_UP and myplayer.jumped == False and myplayer.rect.bottom == 450:
+                myball.x_velocity = 4
+            elif event.key == K_UP and myplayer.jumped == False and myplayer.rect.bottom == 450 and myball.isdribble == False:
+                myplayer.y_velocity = -12
+                #myball.y_velocity = -12
+                myplayer.jumped = True
+            elif event.key == K_DOWN and myplayer.rect.bottom==450:
+                myplayer.x_velocity = 0
+                myplayer.y_velocity = 0
+                myball.isdribble = False
+                #myballSprite = pygame.sprite.RenderClear()      #ball container
+                myball = Ball(myplayer)                         #create ball
+                myballSprite.add(myball)                        #add ball
+            elif event.key == K_i:
+                myball.isdribble = False
+            elif event.key == K_SPACE and myplayer.jumped == False and myplayer.rect.bottom == 450 and myball.isdribble==True:
+                myball.kill()
+                myball = Ball(myplayer)                         #create ball
+                myballSprite.add(myball) 
+                myball.rect.center = (myplayer.rect.midtop)
+                myball.shoot = True
                 myplayer.y_velocity = -12
                 myplayer.jumped = True
-            elif event.key == K_DOWN:
-                pass
+                myball.x_velocity = random.randint(10,20)
+                myball.y_velocity = -25
+
+
         elif event.type == KEYUP:
             if event.key == K_LEFT:
                 myplayer.x_velocity = 0 
+                myball.x_velocity = 0 
             elif event.key == K_RIGHT:
                 myplayer.x_velocity = 0
+                myball.x_velocity = 0
             elif event.key == K_UP:
                 myplayer.y_velocity = 5
+                #myball.y_velocity = 5
                 myplayer.jumped = False
             elif event.key == K_DOWN:
                 pass
+            elif event.key == K_SPACE:
+                myplayer.y_velocity = 5
+                myplayer.jumped = False
         
     myplayerSprite.update()     #update player sprite
+    myballSprite.update()       #update ball sprite
+
     myplayerSprite.clear(screen,background_image) #clean screen
+    myballSprite.clear(screen,background_image)
+    
     myplayerSprite.draw(screen) #draw player sprite
+    myballSprite.draw(screen)   #draw ball sprite
 
     pygame.display.flip() #update the display
     
